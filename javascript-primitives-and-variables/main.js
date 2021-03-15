9de516f8f5679a3d7f6908286ea19d152ca76fdd
Alex Denbaugh <alexdenbaugh@gmail.com>
2021-03-15 10:44:50 -0700

    Completed javascript-primitives-and-variables.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,17 @@
+var fullName = 'Alex Denbaugh';
+var isCool = true;
+var totalPets = 0;
+
+console.log('My name is:', fullName);
+console.log('Am I cool?', isCool);
+console.log('The number of pets I have:', totalPets, ';( sad...');
+
+console.log('fullName is a:', typeof fullName);
+console.log('isCool is a:', typeof isCool);
+console.log('totalPets is a:', typeof totalPets);
+
+var unknown = null;
+console.log('unknown is:', unknown, 'which is of the type:', typeof unknown);
+
+var never;
+console.log('Never is:', never, 'which is of the type:', typeof never);
