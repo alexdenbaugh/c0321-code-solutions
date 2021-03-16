a709df3ee1c3d5d0e048502ff7e13ec9b899934c
Alex Denbaugh <alexdenbaugh@gmail.com>
2021-03-16 13:23:44 -0700

    Completed javascript-data-model.

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,366 @@
+var orderHistory = [
+  [
+    {
+      productDetails: {
+        title: 'JavaScript for Impatient programmers',
+        author: {
+          title: 'Dr.',
+          firstName: 'Axel',
+          lastName: 'Rauschmayer'
+        },
+        image: 'random image',
+        price_b4_SH: 31.55,
+        returnWindow: {
+          day: 7,
+          month: 9,
+          year: 2020
+        },
+        productReview: {
+          stars: 5,
+          featureRating: {
+            feature1: 5,
+            feature2: 2,
+            feature3: 4
+          },
+          photo: 'photo',
+          video: '',
+          review: {
+            headline: 'OMG I LAAAV IT',
+            reviewWriting: 'Buy this, you need it!'
+          }
+        }
+      },
+      orderDetails: {
+        orderStatus: {
+          ordered: {
+            status: true,
+            date: {
+              day: 4,
+              month: 8,
+              year: 2020
+            }
+          },
+          shipped: {
+            status: true,
+            date: {
+              day: 5,
+              month: 8,
+              year: 2020
+            }
+          },
+          outForDelivery: {
+            status: true,
+            date: {
+              day: 6,
+              month: 8,
+              year: 2020
+            }
+          },
+          estimatedArival: {
+            status: true,
+            date: {
+              day: 8,
+              month: 8,
+              year: 2020
+            }
+          },
+          delivered: {
+            status: true,
+            date: {
+              day: 8,
+              month: 8,
+              year: 2020
+            }
+          }
+        },
+        orderNum: '114-3941689-8772232',
+        shipTo: 'JS Masher',
+        invoice: {},
+        orderPriceTotal: 34.00
+      }
+    }
+  ],
+  [
+    {
+      productDetails: {
+        title: 'The Timeless Way of Building',
+        author: {
+          title: '',
+          firstName: 'Christopher',
+          lastName: 'Alexander'
+        },
+        image: 'random image',
+        price_b4_SH: 41.33,
+        returnWindow: {
+          day: 19,
+          month: 8,
+          year: 2020
+        },
+        productReview: {
+          stars: 5,
+          featureRating: {
+            feature1: 5,
+            feature2: 2,
+            feature3: 4
+          },
+          photo: 'photo',
+          video: '',
+          review: {
+            headline: 'OMG I LAAAV IT',
+            reviewWriting: 'Buy this, you need it!'
+          }
+        }
+      },
+      orderDetails: {
+        orderStatus: {
+          ordered: {
+            status: true,
+            date: {
+              day: 19,
+              month: 7,
+              year: 2020
+            }
+          },
+          shipped: {
+            status: true,
+            date: {
+              day: 19,
+              month: 7,
+              year: 2020
+            }
+          },
+          outForDelivery: {
+            status: true,
+            date: {
+              day: 19,
+              month: 7,
+              year: 2020
+            }
+          },
+          estimatedArival: {
+            status: true,
+            date: {
+              day: 20,
+              month: 7,
+              year: 2020
+            }
+          },
+          delivered: {
+            status: true,
+            date: {
+              day: 20,
+              month: 7,
+              year: 2020
+            }
+          }
+        },
+        orderNum: '113-9984268-1280257',
+        shipTo: 'JS Masher',
+        invoice: {},
+        orderPriceTotal: 44.53,
+        noteToCustomer: 'Your package was delivered. It was handed directly to a resident.'
+      }
+    }
+  ],
+  [
+    {
+      productDetails: {
+        title: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+        author: {
+          title: '',
+          firstName: '',
+          lastName: ''
+        },
+        image: 'random image',
+        price_b4_SH: 15.98,
+        returnWindow: {
+          day: 5,
+          month: 8,
+          year: 2020
+        },
+        productReview: {
+          stars: 5,
+          featureRating: {
+            feature1: 5,
+            feature2: 2,
+            feature3: 4
+          },
+          photo: 'photo',
+          video: '',
+          review: {
+            headline: 'OMG I LAAAV IT',
+            reviewWriting: 'Buy this, you need it!'
+          }
+        }
+      },
+      orderDetails: {
+        orderStatus: {
+          ordered: {
+            status: true,
+            date: {
+              day: 4,
+              month: 7,
+              year: 2020
+            }
+          },
+          shipped: {
+            status: true,
+            date: {
+              day: 5,
+              month: 7,
+              year: 2020
+            }
+          },
+          outForDelivery: {
+            status: true,
+            date: {
+              day: 6,
+              month: 7,
+              year: 2020
+            }
+          },
+          estimatedArival: {
+            status: true,
+            date: {
+              day: 7,
+              month: 7,
+              year: 2020
+            }
+          },
+          delivered: {
+            status: true,
+            date: {
+              day: 7,
+              month: 7,
+              year: 2020
+            }
+          }
+        },
+        orderNum: '114-2875557-9059409',
+        shipTo: 'JS Masher',
+        invoice: {},
+        orderPriceTotal: 17.22,
+        noteToCustomer: 'Your package was delivered. It was handed directly to a resident.'
+      }
+    }
+  ],
+  [
+    {
+      product1: {
+        productDetails: {
+          title: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+          author: {
+            title: '',
+            firstName: '',
+            lastName: ''
+          },
+          image: 'random image',
+          price_b4_SH: 94.95,
+          returnWindow: {
+            day: 4,
+            month: 8,
+            year: 2020
+          },
+          productReview: {
+            stars: 5,
+            featureRating: {
+              feature1: 5,
+              feature2: 2,
+              feature3: 4
+            },
+            photo: 'photo',
+            video: '',
+            review: {
+              headline: 'OMG I LAAAV IT',
+              reviewWriting: 'Buy this, you need it!'
+            }
+          }
+        }
+      },
+      product2: {
+        productDetails: {
+          title: 'The Art of Sql',
+          author: {
+            title: '',
+            firstName: 'Stephane',
+            lastName: 'Faroult'
+          },
+          image: 'random image',
+          price_b4_SH: 33.99,
+          returnWindow: {
+            day: 4,
+            month: 8,
+            year: 2020
+          },
+          productReview: {
+            stars: 5,
+            featureRating: {
+              feature1: 5,
+              feature2: 2,
+              feature3: 4
+            },
+            photo: 'photo',
+            video: '',
+            review: {
+              headline: 'OMG I LAAAV IT',
+              reviewWriting: 'Buy this, you need it!'
+            }
+          }
+        }
+      }
+    },
+    {
+      orderStatus: {
+        ordered: {
+          status: true,
+          date: {
+            day: 3,
+            month: 7,
+            year: 2020
+          }
+        },
+        shipped: {
+          status: true,
+          date: {
+            day: 3,
+            month: 7,
+            year: 2020
+          }
+        },
+        outForDelivery: {
+          status: true,
+          date: {
+            day: 4,
+            month: 7,
+            year: 2020
+          }
+        },
+        estimatedArival: {
+          status: true,
+          date: {
+            day: 5,
+            month: 7,
+            year: 2020
+          }
+        },
+        delivered: {
+          status: true,
+          date: {
+            day: 5,
+            month: 7,
+            year: 2020
+          }
+        }
+      },
+      orderNum: '113-2883177-2648248',
+      shipTo: 'JS Masher',
+      invoice: {},
+      orderPriceTotal: 138.93,
+      noteToCustomer: 'Your package was delivered. It was handed directly to a resident.'
+    }
+  ]
+];
+console.log('order1:', orderHistory[0]);
+console.log('order2:', orderHistory[1]);
+console.log('order3:', orderHistory[2]);
+console.log('order4:', orderHistory[3]);
