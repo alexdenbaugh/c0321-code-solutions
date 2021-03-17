34b2c7bc2ce0cc8ba0b88a124277384b0d9a66a0
Alex Denbaugh <alexdenbaugh@gmail.com>
2021-03-17 16:09:04 -0700

    Scrapped and rewrote the orderHistory array literal, it was too confusing to re-organize.

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -1,364 +1,75 @@
 var orderHistory = [
-  [
-    {
-      productDetails: {
-        title: 'JavaScript for Impatient programmers',
-        author: {
-          title: 'Dr.',
-          firstName: 'Axel',
-          lastName: 'Rauschmayer'
-        },
-        image: 'random image',
-        price_b4_SH: 31.55,
-        returnWindow: {
-          day: 7,
-          month: 9,
-          year: 2020
-        },
-        productReview: {
-          stars: 5,
-          featureRating: {
-            feature1: 5,
-            feature2: 2,
-            feature3: 4
-          },
-          photo: 'photo',
-          video: '',
-          review: {
-            headline: 'OMG I LAAAV IT',
-            reviewWriting: 'Buy this, you need it!'
-          }
-        }
-      },
-      orderDetails: {
-        orderStatus: {
-          ordered: {
-            status: true,
-            date: {
-              day: 4,
-              month: 8,
-              year: 2020
-            }
-          },
-          shipped: {
-            status: true,
-            date: {
-              day: 5,
-              month: 8,
-              year: 2020
-            }
-          },
-          outForDelivery: {
-            status: true,
-            date: {
-              day: 6,
-              month: 8,
-              year: 2020
-            }
-          },
-          estimatedArival: {
-            status: true,
-            date: {
-              day: 8,
-              month: 8,
-              year: 2020
-            }
-          },
-          delivered: {
-            status: true,
-            date: {
-              day: 8,
-              month: 8,
-              year: 2020
-            }
-          }
-        },
-        orderNum: '114-3941689-8772232',
-        shipTo: 'JS Masher',
-        invoice: {},
-        orderPriceTotal: 34.00
+  {
+    orderPlaced: 'August 4, 2020',
+    recipient: 'JS Masher',
+    orderCost: 34.00,
+    deliveredOn: 'August 8, 2020',
+    orderNumber: '114-3941689-8772232',
+    items: [
+      {
+        itemTitle: 'JavaScript for impatient programmers',
+        author: 'Dr. Axel Rauschmayer',
+        itemImage: 'image url',
+        returnWindow: 'September 7, 2020',
+        itemCost: 31.55
       }
-    }
-  ],
-  [
-    {
-      productDetails: {
-        title: 'The Timeless Way of Building',
-        author: {
-          title: '',
-          firstName: 'Christopher',
-          lastName: 'Alexander'
-        },
-        image: 'random image',
-        price_b4_SH: 41.33,
-        returnWindow: {
-          day: 19,
-          month: 8,
-          year: 2020
-        },
-        productReview: {
-          stars: 5,
-          featureRating: {
-            feature1: 5,
-            feature2: 2,
-            feature3: 4
-          },
-          photo: 'photo',
-          video: '',
-          review: {
-            headline: 'OMG I LAAAV IT',
-            reviewWriting: 'Buy this, you need it!'
-          }
-        }
-      },
-      orderDetails: {
-        orderStatus: {
-          ordered: {
-            status: true,
-            date: {
-              day: 19,
-              month: 7,
-              year: 2020
-            }
-          },
-          shipped: {
-            status: true,
-            date: {
-              day: 19,
-              month: 7,
-              year: 2020
-            }
-          },
-          outForDelivery: {
-            status: true,
-            date: {
-              day: 19,
-              month: 7,
-              year: 2020
-            }
-          },
-          estimatedArival: {
-            status: true,
-            date: {
-              day: 20,
-              month: 7,
-              year: 2020
-            }
-          },
-          delivered: {
-            status: true,
-            date: {
-              day: 20,
-              month: 7,
-              year: 2020
-            }
-          }
-        },
-        orderNum: '113-9984268-1280257',
-        shipTo: 'JS Masher',
-        invoice: {},
-        orderPriceTotal: 44.53,
-        noteToCustomer: 'Your package was delivered. It was handed directly to a resident.'
+    ]
+  },
+  {
+    orderPlaced: 'July 19, 2020',
+    recipient: 'JS Masher',
+    orderCost: 44.53,
+    deliveredOn: 'July 20, 2020',
+    orderNumber: '113-9984268-1280257',
+    items: [
+      {
+        itemTitle: 'The Timeless Way of Building',
+        author: 'Alexander, Christopher',
+        itemImage: 'image url',
+        returnWindow: 'August 19, 2020',
+        itemCost: 41.33
       }
-    }
-  ],
-  [
-    {
-      productDetails: {
-        title: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
-        author: {
-          title: '',
-          firstName: '',
-          lastName: ''
-        },
-        image: 'random image',
-        price_b4_SH: 15.98,
-        returnWindow: {
-          day: 5,
-          month: 8,
-          year: 2020
-        },
-        productReview: {
-          stars: 5,
-          featureRating: {
-            feature1: 5,
-            feature2: 2,
-            feature3: 4
-          },
-          photo: 'photo',
-          video: '',
-          review: {
-            headline: 'OMG I LAAAV IT',
-            reviewWriting: 'Buy this, you need it!'
-          }
-        }
-      },
-      orderDetails: {
-        orderStatus: {
-          ordered: {
-            status: true,
-            date: {
-              day: 4,
-              month: 7,
-              year: 2020
-            }
-          },
-          shipped: {
-            status: true,
-            date: {
-              day: 5,
-              month: 7,
-              year: 2020
-            }
-          },
-          outForDelivery: {
-            status: true,
-            date: {
-              day: 6,
-              month: 7,
-              year: 2020
-            }
-          },
-          estimatedArival: {
-            status: true,
-            date: {
-              day: 7,
-              month: 7,
-              year: 2020
-            }
-          },
-          delivered: {
-            status: true,
-            date: {
-              day: 7,
-              month: 7,
-              year: 2020
-            }
-          }
-        },
-        orderNum: '114-2875557-9059409',
-        shipTo: 'JS Masher',
-        invoice: {},
-        orderPriceTotal: 17.22,
-        noteToCustomer: 'Your package was delivered. It was handed directly to a resident.'
+    ]
+  },
+  {
+    orderPlaced: 'July 4, 2020',
+    recipient: 'JS Masher',
+    orderCost: 17.22,
+    deliveredOn: 'July 7, 2020',
+    orderNumber: '114-2875557-9059409',
+    items: [
+      {
+        itemTitle: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+        author: null,
+        itemImage: 'image url',
+        returnWindow: 'August 5, 2020',
+        itemCost: 15.98
       }
-    }
-  ],
-  [
-    {
-      product1: {
-        productDetails: {
-          title: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
-          author: {
-            title: '',
-            firstName: '',
-            lastName: ''
-          },
-          image: 'random image',
-          price_b4_SH: 94.95,
-          returnWindow: {
-            day: 4,
-            month: 8,
-            year: 2020
-          },
-          productReview: {
-            stars: 5,
-            featureRating: {
-              feature1: 5,
-              feature2: 2,
-              feature3: 4
-            },
-            photo: 'photo',
-            video: '',
-            review: {
-              headline: 'OMG I LAAAV IT',
-              reviewWriting: 'Buy this, you need it!'
-            }
-          }
-        }
+    ]
+  },
+  {
+    orderPlaced: 'July 3, 2020',
+    recipient: 'JS Masher',
+    orderCost: 138.93,
+    deliveredOn: 'July 5, 2020',
+    orderNumber: '113-2883177-2648248',
+    items: [
+      {
+        itemTitle: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        author: null,
+        itemImage: 'image url',
+        returnWindow: 'August 4, 2020',
+        itemCost: 94.95
       },
-      product2: {
-        productDetails: {
-          title: 'The Art of Sql',
-          author: {
-            title: '',
-            firstName: 'Stephane',
-            lastName: 'Faroult'
-          },
-          image: 'random image',
-          price_b4_SH: 33.99,
-          returnWindow: {
-            day: 4,
-            month: 8,
-            year: 2020
-          },
-          productReview: {
-            stars: 5,
-            featureRating: {
-              feature1: 5,
-              feature2: 2,
-              feature3: 4
-            },
-            photo: 'photo',
-            video: '',
-            review: {
-              headline: 'OMG I LAAAV IT',
-              reviewWriting: 'Buy this, you need it!'
-            }
-          }
-        }
+      {
+        itemTitle: 'The Art of Sql',
+        author: 'Stephanie Faroult',
+        itemImage: 'image url',
+        returnWindow: 'August 4, 2020',
+        itemCost: 33.99
       }
-    },
-    {
-      orderStatus: {
-        ordered: {
-          status: true,
-          date: {
-            day: 3,
-            month: 7,
-            year: 2020
-          }
-        },
-        shipped: {
-          status: true,
-          date: {
-            day: 3,
-            month: 7,
-            year: 2020
-          }
-        },
-        outForDelivery: {
-          status: true,
-          date: {
-            day: 4,
-            month: 7,
-            year: 2020
-          }
-        },
-        estimatedArival: {
-          status: true,
-          date: {
-            day: 5,
-            month: 7,
-            year: 2020
-          }
-        },
-        delivered: {
-          status: true,
-          date: {
-            day: 5,
-            month: 7,
-            year: 2020
-          }
-        }
-      },
-      orderNum: '113-2883177-2648248',
-      shipTo: 'JS Masher',
-      invoice: {},
-      orderPriceTotal: 138.93,
-      noteToCustomer: 'Your package was delivered. It was handed directly to a resident.'
-    }
-  ]
+    ]
+  }
 ];
 console.log('order1:', orderHistory[0]);
 console.log('order2:', orderHistory[1]);
