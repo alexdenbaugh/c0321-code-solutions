9d459cdd4b0ed1a63187fd68d8f6d268a9a2d200
Alex Denbaugh <alexdenbaugh@gmail.com>
2021-03-19 16:29:28 -0700

    Revised array-manipulation-1.

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -1,20 +1,9 @@
 /* exported compact */
-// function compact(array) {
-//   var newArray = [];
-//   for (var i = 0; i < array.length; i++) {
-//     if (array[i] !== false && array[i] !== '' && array[i] !== -0 && array[i] !== 0 && array[i] !== undefined && array[i] !== null) {
-//       if (array[i] === array[i]) {
-//         newArray.push(array[i]);
-//       };
-//     };
-//   };
-//   return newArray;
-// };
 
 function compact(array) {
   var newArray = [];
   for (var i = 0; i < array.length; i++) {
-    if (array[i] > 0 || array[i] < 0 || array[i] === true || (array[i] !== '' && typeof (array[i]) === typeof ('')) || (typeof (array[i]) === typeof ([]) && array[i] !== null)) {
+    if (array[i]) {
       newArray.push(array[i]);
     }
   }
--- array-manipulation-1/src/tail.js
+++ array-manipulation-1/src/tail.js
@@ -2,7 +2,7 @@
 function tail(array) {
   var newArray = [];
   for (var i = 1; i < array.length; i++) {
-    newArray[i - 1] = array[i];
+    newArray.push(array[i]);
   }
   return newArray;
 }
