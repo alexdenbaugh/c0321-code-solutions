ac1c75229bff3573ea172de5f65e4c6ba4e2ae1c
Alex Denbaugh <alexdenbaugh@gmail.com>
2021-03-21 11:23:58 -0700

    Completed array-manipulation-2.

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,15 +1,19 @@
 /* exported chunk */
 
-// function chunk(array, size) {
-//   var newArray = [];
-//   var chuckArray = [];
-//   var remainder = array.length % size;
-//   var j = 0;
-//   var number = (array.length - remainder) / size;
-//   for (var i = 0; i < number; i++) {
-//     while (j <= count) {
-//     chunkArray.push()
-//     };
-//   };
-//   return newArray;
-// };
+function chunk(array, size) {
+  var newArray = [];
+  var chunkArray = [];
+  for (var i = 0; i < array.length; i++) {
+    if (chunkArray.length < size) {
+      chunkArray.push(array[i]);
+    } else if (chunkArray.length === size) {
+      newArray.push(chunkArray);
+      chunkArray = [];
+      chunkArray.push(array[i]);
+    }
+    if (i === array.length - 1) {
+      newArray.push(chunkArray);
+    }
+  }
+  return newArray;
+}
