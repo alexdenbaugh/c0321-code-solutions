5ff0ebba78e241c16be7e00135268d138d3c5c68
Alex Denbaugh <alexdenbaugh@gmail.com>
2021-03-24 13:26:06 -0700

    completed javascript-typing-tutor.

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,22 +1,15 @@
 var $letters = document.querySelectorAll('span');
-var $spanLetter = [];
-for (var i = 0; i < $letters.length; i++) {
-  $spanLetter.push($letters[i].textContent);
-}
 var currentLetter = 0;
 
 document.addEventListener('keydown', function (event) {
-  if (currentLetter >= $spanLetter.length) {
-    return;
-  }
-  if (event.key === $spanLetter[currentLetter]) {
+  if (event.key === $letters[currentLetter].textContent) {
     $letters[currentLetter].className = 'letters green';
     $letters[currentLetter + 1].className = 'letters underline';
     currentLetter++;
   } else {
     $letters[currentLetter].className = 'letters red underline-red';
   }
-  if (event.key === $spanLetter[$spanLetter.length - 1]) {
+  if (event.key === $letters[$letters.length - 1]) {
     $letters[currentLetter + 1].classList.remove('underline');
   }
 });
