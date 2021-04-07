3ca3e25aee90ed7edf95ece143f678490588c808
Alex Denbaugh <alexdenbaugh@gmail.com>
2021-04-07 13:19:19 -0700

    Finished revisions to is-anagram and title-case.

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -2,28 +2,15 @@
 function isAnagram(firstString, secondString) {
   var firstStringArray = firstString.split('');
   var secondStringArray = secondString.split('');
-  var hasASpace = true;
   firstStringArray.sort();
   secondStringArray.sort();
-  while (hasASpace) {
-    if (firstStringArray[0] === ' ') {
-      firstStringArray.shift();
-    } else {
-      hasASpace = false;
-    }
+  firstString = firstStringArray.join('');
+  secondString = secondStringArray.join('');
+  firstString = firstString.trim();
+  secondString = secondString.trim();
+  if (secondString === firstString) {
+    return true;
+  } else {
+    return false;
   }
-  hasASpace = true;
-  while (hasASpace) {
-    if (secondStringArray[0] === ' ') {
-      secondStringArray.shift();
-    } else {
-      hasASpace = false;
-    }
-  }
-  for (var i = 0; i < firstString.length; i++) {
-    if (secondStringArray[i] !== firstStringArray[i]) {
-      return false;
-    }
-  }
-  return true;
 }
--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -2,52 +2,29 @@
 function titleCase(string) {
   var stringArray = string.split(' ');
   var wordString = '';
-  var firstLetter = '';
-  var otherLetters = '';
   var exceptions = ['and', 'or', 'nor', 'but', 'a', 'an', 'the', 'as', 'at', 'by', 'for', 'in', 'of', 'on', 'per', 'to'];
   for (var i = 0; i < stringArray.length; i++) {
     wordString = stringArray[i];
+    wordString = wordString.toLowerCase();
     if (wordString.startsWith('Java') || wordString.startsWith('java')) {
-      firstLetter = wordString[4];
-      firstLetter = firstLetter.toUpperCase();
-      otherLetters = wordString.slice(0, 4);
-      otherLetters = otherLetters + firstLetter;
-      firstLetter = wordString.slice(5);
-      wordString = otherLetters + firstLetter;
-      firstLetter = wordString[0];
-      otherLetters = wordString.slice(1);
-      firstLetter = firstLetter.toUpperCase();
-      wordString = firstLetter + otherLetters;
+      wordString = capitalizeWord(wordString[0], wordString.slice(1, 4)) + capitalizeWord(wordString[4], wordString.slice(5));
     } else if (wordString.toLowerCase() === 'api') {
-      wordString = wordString.toUpperCase();
+      wordString = capitalizeWord(wordString, '');
     } else if (!exceptions.includes(stringArray[i])) {
-      firstLetter = wordString[0];
-      otherLetters = wordString.slice(1);
-      firstLetter = firstLetter.toUpperCase();
-      wordString = firstLetter + otherLetters;
+      wordString = capitalizeWord(wordString[0], wordString.slice(1));
     }
     if (wordString.includes(':')) {
-      firstLetter = wordString[0];
-      otherLetters = wordString.slice(1);
-      firstLetter = firstLetter.toUpperCase();
-      wordString = firstLetter + otherLetters;
-      otherLetters = stringArray[i + 1];
-      firstLetter = otherLetters[0];
-      firstLetter = firstLetter.toUpperCase();
-      otherLetters = otherLetters.slice(1);
-      stringArray[i + 1] = firstLetter + otherLetters;
+      stringArray[i + 1] = capitalizeWord(stringArray[i + 1][0], stringArray[i + 1].slice(1));
     }
     if (wordString.includes('-')) {
-      firstLetter = wordString[0];
-      otherLetters = wordString.slice(1);
-      firstLetter = firstLetter.toUpperCase();
-      wordString = firstLetter + otherLetters;
-      firstLetter = wordString[wordString.indexOf('-') + 1];
-      firstLetter = firstLetter.toUpperCase();
-      otherLetters = wordString.slice(0, wordString.indexOf('-') + 1);
-      wordString = otherLetters + firstLetter + wordString.slice(wordString.indexOf('-') + 2);
+      wordString = wordString.slice(0, wordString.indexOf('-') + 1) + capitalizeWord(wordString[wordString.indexOf('-') + 1], wordString.slice(wordString.indexOf('-') + 2));
     }
     stringArray[i] = wordString;
   }
   return stringArray.join(' ');
+}
+
+function capitalizeWord(firstLetter, otherLetters) {
+  firstLetter = firstLetter.toUpperCase();
+  return firstLetter + otherLetters;
 }
