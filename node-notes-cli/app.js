9b64b048a128644c973700f33e70345d659db216
Alex Denbaugh <alexdenbaugh@gmail.com>
2021-04-29 13:16:28 -0700

    Completed feature 2: User can add a note.

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -1,9 +1,18 @@
+const fs = require('fs');
 const jsonObject = require('./data.json');
 // console.log(process.argv[2])
 if (process.argv[2] === 'read') {
   for (const key in jsonObject.notes) {
     console.log(`${key} : ${jsonObject.notes[key]}`);
   }
+} else if (process.argv[2] === 'create') {
+  jsonObject.notes[`${jsonObject.nextId}`] = process.argv[3];
+  jsonObject.nextId++;
+  fs.writeFile('./data.json', JSON.stringify(jsonObject, null, 2), err => {
+    if (err) {
+      throw err;
+    }
+  });
 } else {
-  console.log('Sorry, incorrect command');
+  console.log('Sorry, incorrect command. Valid commands: "read", "create", "delete", "update"');
 }
