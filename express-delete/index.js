59a4245e77e3de545298102cc97f092f70669d88
Alex Denbaugh <alexdenbaugh@gmail.com>
2021-05-04 11:10:47 -0700

    Completed express-delete.

---
+++ express-delete/index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+
+const grades = {
+  12: {
+    id: 12,
+    name: 'Tim Berners-Lee',
+    course: 'HTML',
+    score: 95
+  },
+  47: {
+    id: 47,
+    name: 'Brendan Eich',
+    course: 'JavaScript',
+    score: 100
+  },
+  273: {
+    id: 273,
+    name: 'Forbes Lindsay',
+    course: 'JavaScript',
+    score: 92
+  }
+};
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('Listening on port 3000!');
+});
+
+app.get('/api/grades/', (req, res, next) => {
+  const array = [];
+  for (const k in grades) {
+    array.push(grades[k]);
+  }
+  res.json(array);
+  next();
+});
+
+app.delete('/api/grades/:id', (req, res) => {
+  for (const key in grades) {
+    if (key === req.params.id) {
+      delete grades[key];
+      res.sendStatus(204);
+      break;
+    }
+  }
+});
