e48a93300dfbf133fba709344a0f0e4079b5de04
Alex Denbaugh <alexdenbaugh@gmail.com>
2021-05-13 17:38:21 -0700

    Completed react-events-and-state.

---
+++ react-events-and-state/src/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class CustomButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isClicked: false };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({ isClicked: true });
+  }
+
+  render() {
+    if (this.state.isClicked) {
+      return (
+        <button onClick={this.handleClick}>Thanks!</button>
+      );
+    } else {
+      return (
+        <button onClick={this.handleClick}>Click Me!</button>
+      );
+    }
+  }
+}
+
+ReactDOM.render(
+  <CustomButton />,
+  document.querySelector('#root')
+);
