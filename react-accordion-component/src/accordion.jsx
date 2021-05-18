c220c2789d388804ab88d0961f9d94968560a007
Alex Denbaugh <alexdenbaugh@gmail.com>
2021-05-17 23:20:48 -0700

    Completed react-accordion-component.

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -3,19 +3,28 @@ import React from 'react';
 class Accordion extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { change: false };
     this.TopicList = this.TopicList.bind(this);
     this.handleClick = this.handleClick.bind(this);
     this.ListItem = this.ListItem.bind(this);
   }
 
   handleClick(event) {
-    const x = 2;
-    return x;
-    // console.log(event, this.props);
+    if (this.state.change) {
+      this.setState({ change: false });
+    } else {
+      this.setState({ change: true });
+    }
+    for (let i = 0; i < this.props.topics.length; i++) {
+      if (this.props.topics[i].header === event.target.innerText && this.props.topics[i].selected === false) {
+        this.props.topics[i].selected = true;
+      } else {
+        this.props.topics[i].selected = false;
+      }
+    }
   }
 
   ListItem(topic) {
-    // console.log('inside ListItem', topic)
     if (topic.value.selected) {
       return (
         <>
@@ -37,11 +46,8 @@ class Accordion extends React.Component {
   }
 
   TopicList(list) {
-    // console.log(list)
     const topics = list.list;
-    // console.log('topics:', topics)
     const topicAccordion = topics.map(topic => {
-      // console.log('one-topic', topic);
       return (
         <this.ListItem key={topic.header} value={topic} />
       );
