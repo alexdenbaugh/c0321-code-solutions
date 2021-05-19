bc1d14371664dd11edf450a69a21e1d26a3e6801
Alex Denbaugh <alexdenbaugh@gmail.com>
2021-05-19 08:19:50 -0700

    In Progress.

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -3,65 +3,68 @@ import React from 'react';
 class Accordion extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { change: false };
-    this.TopicList = this.TopicList.bind(this);
+    this.state = { openedTopic: null };
     this.handleClick = this.handleClick.bind(this);
-    this.ListItem = this.ListItem.bind(this);
   }
 
   handleClick(event) {
-    if (this.state.change) {
-      this.setState({ change: false });
-    } else {
-      this.setState({ change: true });
-    }
-    for (let i = 0; i < this.props.topics.length; i++) {
-      if (this.props.topics[i].header === event.target.innerText && this.props.topics[i].selected === false) {
-        this.props.topics[i].selected = true;
+    // console.log('clicked', event.target)
+    const openedTopic = this.props.topics.filter(topic => {
+      if (topic.header === event.target.innerText && topic !== this.state.openedTopic) {
+        return true;
       } else {
-        this.props.topics[i].selected = false;
+        return false;
       }
-    }
-  }
-
-  ListItem(topic) {
-    if (topic.value.selected) {
-      return (
-        <>
-          <div onClick={this.handleClick} className='header'>
-            <h2>{topic.value.header}</h2>
-          </div>
-          <div className="details">
-            <p>{topic.value.details}</p>
-          </div>
-        </>
-      );
-    } else {
-      return (
-        <div onClick={this.handleClick} className='header'>
-          <h2>{topic.value.header}</h2>
-        </div>
-      );
-    }
-  }
-
-  TopicList(list) {
-    const topics = list.list;
-    const topicAccordion = topics.map(topic => {
-      return (
-        <this.ListItem key={topic.header} value={topic} />
-      );
     });
-    return (
-      <>
-        { topicAccordion }
-      </>
-    );
+    if (openedTopic === []) {
+      this.setState({ openedTopic: null });
+    } else {
+      this.setState({ openedTopic: openedTopic });
+    }
   }
 
   render() {
+    // console.log('TopicList element:', <TopicList list={this.props.topics} />)
     return (
-      <this.TopicList list={this.props.topics} />
+      <TopicList list={this.props.topics} openedTopic={this.state.openedTopic} />
+    );
+  }
+}
+
+function TopicList(props) {
+  // console.log('Im inside TopicList', props.list)
+  const topicAccordion = props.list.map(topic => {
+    // console.log('topic', topic)
+    return (
+      <ListItem key={topic.header} topic={topic} openedTopic={props.openedTopic} />
+    );
+  });
+  // console.log('topicAccordian in TopicList', topicAccordion)
+  return (
+    <>
+      { topicAccordion }
+    </>
+  );
+}
+
+function ListItem(props) {
+  // console.log("I'm inside ListItem", props.openedTopic)
+  if (props.topic === props.openedTopic) {
+    return (
+      <>
+        <div onClick={Accordion.handleClick} className='header'>
+          <h2>{props.topic.header}</h2>
+        </div>
+        <div className="details">
+          <p>{props.topic.details}</p>
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <div onClick={Accordion.handleClick} className='header'>
+        <h2>{props.topic.header}</h2>
+      </div>
     );
   }
 }
--- react-accordion-component/src/index.jsx
+++ react-accordion-component/src/index.jsx
@@ -5,22 +5,17 @@ import Accordion from './accordion';
 const topics = [
   {
     header: 'Hypertext Markup Language',
-    details: 'Hypertext Markupp Language (HTML) is the standard markup language for creating web pages and web applications. With Cascading Style Sheets (CSS) and JavaScript, it forms a triad of cornerstone technologies for the World Wide Web.',
-    selected: false
+    details: 'Hypertext Markupp Language (HTML) is the standard markup language for creating web pages and web applications. With Cascading Style Sheets (CSS) and JavaScript, it forms a triad of cornerstone technologies for the World Wide Web.'
   },
   {
     header: 'Cascading Style Sheets',
-    details: 'Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document written in a markup language like HTML. CSS is a cornerstone technology of the World Wide Web, alongside HTML and JavaScript.',
-    selected: false
+    details: 'Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document written in a markup language like HTML. CSS is a cornerstone technology of the World Wide Web, alongside HTML and JavaScript.'
   },
   {
     header: 'JavaScript',
-    details: 'JavaScript, often abbreviated as JS, is a high-level, interpreted programming language that conforms to the ECMAScript specification. JavaScript has curly-bracket syntax, dynamic typing, prototype-based object-orientation, and first-class functions.',
-    selected: false
+    details: 'JavaScript, often abbreviated as JS, is a high-level, interpreted programming language that conforms to the ECMAScript specification. JavaScript has curly-bracket syntax, dynamic typing, prototype-based object-orientation, and first-class functions.'
   }
 ];
-
-// console.log(Accordion)
 
 ReactDOM.render(
   <Accordion topics={topics}/>,
