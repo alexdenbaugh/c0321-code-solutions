5d564884cb5ca25f5b1c3741876be05f945b550c
Alex Denbaugh <alexdenbaugh@gmail.com>
2021-05-21 12:56:42 -0700

    In progress of extra question.

--- node-fs-readfile/cat.js
+++ node-fs-readfile/cat.js
@@ -1,11 +1,19 @@
 const fs = require('fs');
 
+// let logArray = []
+// for (let i = 2; i < process.argv.length; i++) {
+//   logArray.push('')
+// }
+
 for (let i = 2; i < process.argv.length; i++) {
   fs.readFile(`./${process.argv[i]}`, 'utf8', (err, data) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(data);
-    }
+    // if (err) {
+    console.log(err);
+    // } else {
+    //   logArray[i-2] = data;
+    // }
+    // if (i === process.argv.length - 1) {
+    //   console.log(...logArray)
+    // }
   });
 }
